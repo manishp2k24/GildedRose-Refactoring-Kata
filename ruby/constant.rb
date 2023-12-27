79bdf50eeb3f8043cd6fda2d731df25320ee8ea4
manishp2k24 <manish_prajapat@softude.com>
2023-12-27 16:40:22 +0530

    Added constants to the Constant class for common item names like 'Conjured', 'Backstage passes to a TAFKAL80ETC concert', 'Aged Brie', and 'Sulfuras, Hand of Ragnaros'. These constants improve code readability and centralize item names for better maintainability.

---
+++ ruby/constant.rb
@@ -0,0 +1,6 @@
+class Constant
+  CONJURED_ITEM = 'Conjured'.freeze
+  BACKSTAGE_CONCERT_ITEM = 'Backstage passes to a TAFKAL80ETC concert'.freeze
+  AGED_BRIE_ITEM = 'Aged Brie'.freeze
+  SULFURAS_ITEM = 'Sulfuras, Hand of Ragnaros'.freeze
+end
